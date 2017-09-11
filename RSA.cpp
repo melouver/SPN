6a8e293646149d5157ad2d79c36cb666937fb044
melouver <melouverr@gmail.com>
2017-09-12 07:17:44 +0800

    add rsa file

---
+++ RSA.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 李玉哲 on 11/09/2017.
+//
+
+#include <stdio.h>
+#include <gmp.h>
+#include <stdarg.h>
+
+
+void parameter_generator();
+
+
+int main () {
+    mpz_t p, q;
+
+
+
+    mpz_clears(p, q);
+
+
+}
